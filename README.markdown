af3712e7ba4dc17a82ecc1a270352c4ddeda0a60
Xavier Lacot <xavier@lacot.org>
2010-11-15 21:01:37 +0100

    Initial public release. Features a simple ActiveRecord implementation, along with an OOP query API

---
+++ README.markdown
@@ -0,0 +1,211 @@
+# joli.js, a light js ORM for Appcelerator Titanium
+
+## Presentation of joli.js
+joli.js is a simple ORM for [Appcelerator Titanium mobile](http://www.appcelerator.com/products/titanium-mobile-application-development/) projects. It was built borrowing large parts of the code of [JazzRecord](http://www.jazzrecord.org/), a more general and complex javascript ORM. Praise and kudos to them!
+
+## What does "joli" stand for?
+"joli" means in French "nice", "tiny". Just what joli.js tries to be.
+
+## Download and install
+Just grab joli.js (a single file), and include it in your Titanium project using
+
+    Titanium.include('joli.js');
+
+The sources are [available on GitHub](https://github.com/xavierlacot/joli.js).
+
+## Configuration
+
+### Database connection creation
+There is one single required step in the configuration of joli.js: configuring the database name. This can be done in only one line, which has to be put before every call to joli.js's API:
+
+    joli.connection = new joli.Connection('your_database_name');
+
+### Models configuration
+Prior inserting data and querying your models, you must declare these models. This is done by instantiating the class "joli.model":
+
+    var city = new joli.model({
+      table:    'city',
+      columns:  {
+        id:                 'INTEGER',
+        name:               'TEXT',
+        description:        'TEXT'
+      }
+    });
+
+If your application uses a lot of models, I advice to bond all of these in a `models` variable, which will contain every models:
+
+    var models = (function() {
+      m.human = new joli.model({
+        table:    'human',
+        columns:  {
+          id:                 'INTEGER',
+          city_id:            'INTEGER',
+          first_name:         'TEXT',
+          last_name:          'TEXT'
+        },
+        methods: {
+          move:  function(newCityName) {
+            // search for the city id
+            var city = joli.models.get('city').findOneBy('name', newCityName);
+
+            if (!city) {
+              throw 'Could not find a city with this name!';
+            } else {
+              this.city_id = city.id;
+            }
+          }
+        }
+      });
+
+      m.city = new joli.model({
+        table:    'city',
+        columns:  {
+          id:                 'INTEGER',
+          country_id:         'INTEGER',
+          name:               'TEXT',
+          description:        'TEXT'
+        }
+      });
+
+      m.country = new joli.model({
+        table:    'country',
+        columns:  {
+          id:                 'INTEGER',
+          name:               'TEXT'
+        }
+      });
+
+      return m;
+    })();
+
+
+The parameters array, which allows to configure a model, may contain several keys:
+
+* `table`: the table name,
+* `columns`: the name of the various columns proposed by the model. For each of them, it is required to specify their type (`INTEGER`, `TEXT` or `FLOAT`),
+* `methods`: a table of methods, in order to extend the model (see the `move` method upper)
+
+## Usage
+This section describes the way on how to use joli.js.
+
+### Tables initialisation
+At the first launch of an application on a device, it is required to create the tables associated with the models, with the required fields. Of course, joli.js helps initialising the database: simple call the `joli.models.initialize()` method once the models have been defined:
+
+    var city = new joli.model({
+      table:    'city',
+      columns:  {
+        id:                 'INTEGER',
+        name:               'TEXT',
+        description:        'TEXT'
+      }
+    });
+
+    joli.models.initialize();
+
+
+### Data insertion
+Inserting data can be done using the `newRecord()` method of a model:
+
+    // create the record (not persisted)
+    var john = models.human.newRecord({
+      first_name: 'John',
+      last_name: 'Doe'
+    });
+
+    // move him to New York
+    john.move('New York');
+
+    // persist it
+    john.save();
+
+You may also want to create a record using the instance class directly:
+
+    var john = new joli.record(models.human);
+    john.fromArray({
+      first_name: 'John',
+      last_name: 'Doe'
+    });
+
+    // move him to New York
+    john.move('New York');
+
+    // persist it
+    john.save();
+
+The first method i however adviced, as it performs some checks on the existence of the columns.
+
+
+### Data retrieval and Query API
+Retrieving data is often a pain. For all the models, joli.js implements some magic finders in the model classes:
+
+* `findBy(field, value)` allows to retrieve a list of the records having a specific value for one of its fields
+* `findById(field, value)` allows to retrieve a list of the records having a specific id
+* `findOneBy(field, value)` allows to retrieve one record having a specific value for one of its fields. If several records match the criteria, then onl the first one will be returned
+* `findOneById(field, value)` allows to retrieve one record having a specific id
+
+But of course, you will want to perform more complex searches. This is where the query API enters in the dance. This query API allows to create `joli.query` objects, which are turned into real SQL queries by the ORM when executing the query.
+
+This is particularly powerful when you want to add restrictions to the query in conditional statements:
+
+    var q = new joli.query()
+      .select('human.*')
+      .from('human')
+      .order(['last_name desc', 'first_name asc']);
+
+    if (win.city_id) {
+      q.where('city_id = ?', win.city_id);
+    }
+
+    if (win.last_name) {
+      q.where('last_name LIKE ?', '%' + win.last_name + '%');
+    }
+
+    if (win.city_name) {
+      q.where('city.name = ?', win.city_name);
+      q.join('city', 'city.id', 'human.city_id');
+    }
+
+    var humans = q.execute();
+
+The Query API supports lots of things. Just have a check at the joli.query class!
+
+In some cases however, you will find this way of querying your models just to long, and you'll prefer an other alternative syntax (Criteria-style querying API):
+
+    var humans = models.human.all({
+      where: {
+        'city_id = ?': win.city_id
+      },
+      order: ['last_name desc', 'first_name asc']
+    });
+
+
+## Internals
+joli.js is made of several classes:
+
+* `joli`, which is a convenience class for storing utilities,
+* `joli.Connection`, which handles the real connection to the database,
+* `joli.model`, which allows to perform some operations on a model,
+* `joli.Models`, which acts as a hashmap of the models, and allows to initialise the database,
+* `joli.query`, allows to write queries in a OOP style,
+* and, finally, `joli.record` represents a record and contains useful methods.
+
+You may want to override some of these classes for your convenience (for instance, for implementing an automatic synchronisation with a REST web service)... Just feel free to contribute back your changes!
+
+## Howtos
+This section gives some clues about how to use joli.js. It will be updated when questions will be sent to me about joli.js.
+
+### Lost the tables reference
+Would you have a specific part of your code where your models can not be accessed using the `models` object, it is still possible to access it using the method `joli.models.get()`. In other words:
+
+    models.human == joli.models.get('human');
+
+## Credits and support
+joli.js has been developed by [Xavier Lacot](http://lacot.org/) and is
+licensed under the MIT license.
+
+Please use GitHub in order to report bugs, but you may also ask for help on how to use joli.js by sending me a mail directly. My email address is xavier@lacot.org.
+
+## Changelog
+
+### Version 0.1 - 2010-11-15
+Initial public release. Features a simple ActiveRecord implementation, along with an OOP query API.
---
+++ joli.js
@@ -0,0 +1,641 @@
+var joli = {
+  each: function(collection, iterator, bind) {
+    switch (joli.getType(collection)) {
+      case "array":
+        for (var i = 0, l = collection.length; i < l; i++) {
+          iterator.call(bind, collection[i], i);
+        }
+        break;
+      case "object":
+        for (var property in collection) {
+          if (collection.hasOwnProperty(property)) {
+            iterator.call(bind, collection[property], property);
+          }
+        }
+        break;
+    }
+  },
+
+  extend: function(baseClass, options) {
+    if (!this.options) {
+      this.options = {};
+    }
+
+    this.parent = new baseClass(options);
+
+    for (var prop in this.parent) {
+      this[prop] = this[prop] || this.parent[prop];
+    }
+
+    // copy base options over
+    for (var opt in this.parent.options) {
+      this.options[opt] = this.options[opt] || this.parent.options[opt];
+    }
+  },
+
+  getType: function(obj) {
+    if (typeof obj === "undefined" || obj === null || (typeof obj === "number" && isNaN(obj))) {
+      return false;
+    } else if (obj.constructor === Array) {
+      return "array";
+    } else {
+      return typeof obj;
+    }
+  },
+
+  jsonParse: function(json) {
+    return eval('(' + json + ')');
+  },
+
+  merge: function() {
+    var mergedObject = {};
+
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      var object = arguments[i];
+
+      if (joli.getType(object) !== "object") {
+        continue;
+      }
+
+      for (var prop in object) {
+        var objectProp = object[prop], mergedProp = mergedObject[prop];
+
+        if (mergedProp && joli.getType(objectProp) === "object" && joli.getType(mergedProp) === "object") {
+          mergedObject[prop] = joli.merge(mergedProp, objectProp);
+        } else {
+          mergedObject[prop] = objectProp;
+        }
+      }
+    }
+    return mergedObject;
+  },
+
+  setOptions: function(options, defaults) {
+    if (!options) {
+      options = {};
+    }
+
+    if (!this.options) {
+      this.options = {};
+    }
+
+    mergedOptions = joli.merge(defaults, options);
+
+    for (var opt in defaults) {
+      this.options[opt] = mergedOptions[opt];
+    }
+  },
+
+  toQueryString: function(obj) {
+    var queryStringComponents = [];
+
+    if (!obj) {
+      obj = {};
+    }
+
+    joli.each(obj, function(val, key) {
+      var result = null;
+
+      switch (joli.getType(val)) {
+        case "object":
+          result = joli.toQueryString(val);
+          break;
+        case "array":
+          result = joli.toQueryString(val);
+          break;
+        default:
+          result = encodeURIComponent(val);
+      }
+
+      if (result) {
+        queryStringComponents.push(key + '=' + result);
+      }
+    });
+
+    return '[' + queryStringComponents.join("&") + ']';
+  },
+
+  typeValue: function(val) {
+    if (!joli.getType(val)) {
+      return "NULL";
+    } else {
+      if (joli.getType(val) === "string") {
+        // escape single quotes and dollar signs.
+        // quotes are escaped for SQLite
+        val = val.replace(/'/g, "''");
+        // dollar signs are escaped for use in calling str.replace in JazzRecord.replaceAndClean()
+        val = val.replace(/\$/g, "$$$$");
+        val = "'" + val + "'";
+      } else if (joli.getType(val) === "boolean") {
+        if (val) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    }
+
+    return val;
+  }
+};
+
+
+/**
+ * Connection
+ */
+joli.Connection = function(database) {
+  this.dbname = database;
+  this.database = Titanium.Database.open(this.dbname);
+  this.database.execute('PRAGMA read_uncommitted=true');
+};
+
+joli.Connection.prototype = {
+  execute: function(query) {
+    // Titanium.API.log(query);
+    return this.database.execute(query);
+  },
+
+  lastInsertRowId: function() {
+    return this.database.lastInsertRowId;
+  }
+};
+
+
+/**
+ * Model description
+ */
+joli.model = function(options) {
+  var defaults = {
+    table: '',
+    columns: {}
+  };
+
+  if (options.methods) {
+    joli.each(options.methods, function(method, name) {
+      this[name] = method;
+    }, this);
+  }
+
+  joli.setOptions.call(this, options, defaults);
+  this.table = this.options.table;
+
+  if (!joli.models.has(this.table)) {
+    joli.models.set(this.table, this);
+  }
+};
+
+joli.model.prototype = {
+  all: function(constraints) {
+    var q = new joli.query().select().from(this.table);
+
+    if (!constraints) {
+      constraints = {};
+    }
+
+    if (constraints.where) {
+      joli.each(constraints.where, function(value, field) {
+        q.where(field, value);
+      });
+    }
+
+    if (constraints.order) {
+      q.order(constraints.order);
+    }
+
+    return q.execute();
+  },
+
+  count: function(constraints) {
+    var q = new joli.query().count().from(this.table);
+
+    if (!constraints) {
+      constraints = {};
+    }
+
+    if (constraints.where) {
+      joli.each(constraints.where, function(value, field) {
+        q.where(field, value);
+      });
+    }
+
+    return q.execute();
+  },
+
+  // no callbacks, more efficient
+  deleteRecords: function(id) {
+    var q = new joli.query().destroy().from(this.table);
+
+    if (joli.getType(id) === 'number') {
+      q.where('id = ?', id);
+    } else if(joli.getType(id) === 'array') {
+      q.whereIn('id', id);
+    }
+
+    return q.execute();
+  },
+
+  exists: function(id) {
+    var count = new joli.query().count().from(this.table).where('id = ?', id).execute();
+    return (count > 0);
+  },
+
+  findBy: function(field, value) {
+    return new joli.query().select().from(this.table).where(field + ' = ?', value).execute();
+  },
+
+  findById: function(value) {
+    return this.findBy('id', value);
+  },
+
+  findOneBy: function(field, value) {
+    var result = new joli.query().select().from(this.table).where(field + ' = ?', value).limit(1).execute();
+
+    if (result.length == 0) {
+      return false;
+    } else {
+      return result[0];
+    }
+  },
+
+  findOneById: function(value) {
+    return this.findOneBy('id', value);
+  },
+
+  getColumns: function() {
+    return this.options.columns;
+  },
+
+  newRecord: function(values) {
+    if (!values) {
+      values = {};
+    }
+
+    var data = {};
+
+    joli.each(this.options.columns, function(colType, colName) {
+      data[colName] = values[colName] || null;
+    });
+
+    var record = new joli.record(this).fromArray(data);
+
+    record.isNew = function() {
+      return true;
+    };
+
+    return record;
+  },
+
+  save: function(data) {
+    if (data.data.length == 0) {
+      return;
+    }
+
+    var q = new joli.query();
+
+    if (data.originalData) {
+      // existing record
+      q.update(this.table).set(data.data).where('id = ?', data.originalData.id);
+    } else {
+      // new record
+      q.insertInto(this.table).values(data.data);
+    }
+
+    return q.execute();
+  }
+};
+
+joli.Models = function() {
+  this.models = {};
+};
+
+joli.Models.prototype = {
+  get: function(table) {
+    return this.models[table];
+  },
+
+  has: function(table) {
+    if (this.models[table]) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  initialize: function() {
+    joli.each(this.models, function(model, modelName) {
+      var columns = [];
+
+      joli.each(model.options.columns, function(type, name) {
+        columns.push(name + ' ' + type);
+      });
+      var query = 'CREATE TABLE IF NOT EXISTS ' + modelName + ' (' + columns.join(', ') + ')';
+      joli.connection.execute(query);
+    });
+  },
+
+  set: function(table, model) {
+    this.models[table] = model;
+  }
+};
+
+joli.models = new joli.Models();
+
+joli.query = function() {
+  this.data = {
+    from:            null,
+    join:            [],
+    limit:           null,
+    operation:       null,
+    order:           [],
+    select_columns:  '*',
+    set:             [],
+    values:          [],
+    where:           null
+  };
+};
+
+joli.query.prototype = {
+  count: function() {
+    this.data.operation = 'count';
+    return this;
+  },
+
+  destroy: function() {
+    this.data.operation = 'delete';
+    return this;
+  },
+
+  execute: function() {
+    return this.executeQuery(this.getQuery());
+  },
+
+  executeQuery: function(query) {
+    var rows;
+
+    switch (this.data.operation) {
+      case 'count':
+        rows = joli.connection.execute(query);
+        return this.getCount(rows);
+      case 'insert_into':
+        joli.connection.execute(query);
+        return joli.connection.lastInsertRowId();
+      case 'select':
+        rows = joli.connection.execute(query);
+        return this.hydrate(rows);
+      default:
+        return joli.connection.execute(query);
+    }
+  },
+
+  from: function(table) {
+    this.data.from = table;
+    return this;
+  },
+
+  getCount: function(rows) {
+    var result;
+
+    if (0 == rows.rowCount) {
+      result = 0;
+    } else {
+      result = rows.fieldByName('total');
+    }
+
+    rows.close();
+    return result;
+  },
+
+  getOperation: function() {
+    switch (this.data.operation) {
+      case 'count':
+        return 'select count(*) as total from ' + this.data.from;
+      case 'delete':
+        return 'delete from ' + this.data.from;
+      case 'insert_into':
+        return 'insert into ' + this.data.from + ' (' + this.data.set.join(', ') + ') values (' + this.data.values.join(', ') + ')';
+      case 'select':
+        var join = '';
+
+        if (this.data.join.length > 0) {
+          joli.each(this.data.join, function(value, key) {
+            join = join + ' left outer join ' + value[0] + ' ON ' + value[1] + '=' + value[2];
+          });
+        }
+
+        return 'select ' + this.data.select_columns + ' from ' + this.data.from + join;
+      case 'update':
+        return 'update ' + this.data.from + ' set ' + this.data.values.join(', ');
+      default:
+        throw("Operation type Error. joli.query operation type must be an insert, a delete, a select or an update.");
+    }
+  },
+
+  getQuery: function() {
+    var query = this.getOperation();
+
+    if (this.data.where) {
+      query += ' where ' + this.data.where;
+    }
+
+    if (this.data.order.length > 0) {
+      query += ' order by ' + this.data.order.join(', ');
+    }
+
+    if (this.data.limit) {
+      query += ' limit ' + this.data.limit;
+    }
+
+    return query;
+  },
+
+  hydrate: function(rows) {
+    var result = [];
+
+    if (!rows) {
+      return result;
+    }
+
+    //Titanium.API.log('hydrating ' + rows.rowCount + ' rows.');
+    var i;
+    var rowData;
+
+    if (Titanium.Platform.name != 'android') {
+      var fieldCount = rows.fieldCount();
+    } else {
+      var fieldCount = rows.fieldCount;
+    }
+
+    while (rows.isValidRow()) {
+      i = 0;
+      rowData = {};
+
+      while (i < fieldCount) {
+        rowData[rows.fieldName(i)] = rows.field(i);
+        i++;
+      }
+
+    	result.push(rowData);
+    	rows.next();
+    }
+
+    rows.close();
+    return result;
+  },
+
+  insertInto: function(table) {
+    this.data.operation = 'insert_into';
+    this.data.from = table;
+    return this;
+  },
+
+  join: function(table, local_id, foreign_id) {
+    this.data.join.push([table, local_id, foreign_id]);
+    return this;
+  },
+
+  limit: function(limit) {
+    this.data.limit = limit;
+    return this;
+  },
+
+  order: function(order) {
+    if ('string' == joli.getType(order)) {
+      order = [order];
+    }
+
+    this.data.order = order;
+    return this;
+  },
+
+  select: function(columns) {
+    this.data.operation = 'select';
+
+    if (columns) {
+      this.data.select_columns = columns;
+    }
+
+    return this;
+  },
+
+  set: function(values) {
+    joli.each(values, function(expression, value) {
+      this.data.set.push(value + ' = ' + joli.typeValue(expression));
+    }, this);
+    return this;
+  },
+
+  update: function(table) {
+    this.data.operation = 'update';
+    this.data.from = table;
+    return this;
+  },
+
+  values: function(values) {
+    joli.each(values, function(expression, value) {
+      this.data.set.push(value);
+      this.data.values.push(joli.typeValue(expression));
+    }, this);
+    return this;
+  },
+
+  where: function(expression, value) {
+    if (null != this.data.where) {
+      this.data.where += ' and ';
+    } else {
+      this.data.where = '';
+    }
+
+    this.data.where += expression.replace(/\?/gi, '"' + value + '"');
+    return this;
+  },
+
+  whereIn: function(expression, value) {
+    if (null != this.data.where) {
+      this.data.where += ' and ';
+    } else {
+      this.data.where = '';
+    }
+
+    this.data.where += expression + ' IN ' + value;
+    return this;
+  }
+};
+
+joli.record = function(table) {
+  this._options = {
+    table: table,
+    columns: table.getColumns()
+  };
+  this._data = {};
+};
+
+joli.record.prototype = {
+  destroy: function() {
+    if (!this.id) {
+      throw("Unsaved record cannot be destroyed");
+    } else {
+      this._options.table.deleteRecords(this.id);
+    }
+  },
+
+  fromArray: function(data) {
+    if (data.id) {
+      this._originalData = {};
+      this.isNew = function() {
+        return false;
+      };
+    } else {
+      this._originalData = null;
+      this.isNew = function() {
+        return true;
+      };
+    }
+
+    joli.each(this._options.columns, function(colType, colName) {
+      this[colName] = null;
+      this[colName] = data[colName];
+      this._data[colName] = null;
+      this._data[colName] = data[colName];
+
+      if (this._originalData) {
+        this._originalData[colName] = data[colName];
+      }
+    }, this);
+
+    return this;
+  },
+
+  isChanged: function() {
+    if (this.isNew()) {
+      return false;
+    }
+
+    return !(this.id && joli.toQueryString(this._data) === joli.toQueryString(this._originalData));
+  },
+
+  save: function() {
+    var data = { data: this._data };
+
+    if (this.isChanged()) {
+      data.originalData = this._originalData;
+      this._options.table.save(data);
+    } else if(this.isNew()) {
+      this._data.id = this._options.table.save(data);
+    }
+
+    // overwrite original data so it is no longer "dirty" OR so it is no longer new
+    this._originalData = {};
+
+    joli.each(this._options.columns, function(colType, colName) {
+      this._originalData[colName] = this._data[colName];
+    }, this);
+
+    this._data = this._originalData;
+
+    this.isNew = function() {
+      return false;
+    };
+
+    return true;
+  }
+};
